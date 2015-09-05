1886792bf29481378973af8d4e992c861f29b299
Hillbp <brandonphill1@gmail.com>
2015-09-05 01:58:44 -0400

    Edited Person1.java and AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,10 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	    String first = new String(input.substring(0,1));
+        String last = new String(input.substring(2));
+        String result = new String(last + first);
+	  return result;
 	}
 	
 	/**
